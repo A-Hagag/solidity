f5218a0f89795a5837f7fe5dc9bfa9a4c2ba8267
A-Hagag <ahmedhagag.work@gmail.com>
2025-08-09 00:43:57 +0300

    begin with solidity

---
+++ sum.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.30;
+
+contract Hello {
+
+    // this is a single line comment
+
+    /*
+    this 
+    is
+    a
+    multiple 
+    line
+    comment
+    */
+
+    // Data types in solidity : string , integer , boolean
+    string carName = "Honda";
+}
